1b0a2bf5a5ab07c3ead202b0c67a3dec42112206
ClassicalMoser <69487098+ClassicalMoser@users.noreply.github.com>
2025-08-16 20:02:52 -0500

    Some basic validation functions

--- src/domain/mod.rs
+++ src/domain/mod.rs
@@ -4,10 +4,10 @@ use uuid::Uuid;
 /// Pure domain logic, no external dependencies, no ECS, no Bevy
 pub mod primitives;
 /// Validation functions for geometry integrity
-pub mod validation;
+pub mod validations;
 
 pub use primitives::*;
-pub use validation::*;
+pub use validations::*;
 
 /// Constant to define unit size for coordinate system
 pub const METERS_PER_UNIT: f32 = 1.0;
